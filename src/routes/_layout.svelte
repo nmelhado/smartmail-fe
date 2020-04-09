49ff272598587a7b3257fde74ea8929c08e08859
Nicholas Melhado <n.melhado@gmail.com>
2020-04-09 17:12:20 -0400

    add yamls for cloud and app build

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -17,12 +17,26 @@
 		padding: 2em 2em 4em;
 		margin: 0 auto;
     box-sizing: border-box;
-	}
+  }
+  
+  /* #contact_banner {
+    position: relative;
+    min-width: 100%;
+    width: auto;
+    height: auto;
+    left: 50%;
+    max-height: 200px;
+    overflow-y: hidden;
+    overflow-x: hidden;
+    transform: translate(-50%, 0);
+  } */
 </style>
 
 <div id="mainContent">
   <Nav {segment}/>
-
+  <!-- {#if segment == "contact"}
+    <img id="contact_banner" src="contact_banner.jpg" alt="contact banner" />
+  {/if} -->
   <main>
     <slot></slot>
   </main>
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import {Datastore} from '@google-cloud/datastore';
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
@@ -6,14 +7,14 @@ import session from 'express-session'
 import sessionFileStore from 'session-file-store'
 import bodyParser from 'body-parser'
 
-const FileStore = sessionFileStore(session)
-
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(bodyParser.json())
-  .use(session({
+if (dev) {
+  const FileStore = sessionFileStore(session)
+  polka() // You can also use Express
+    .use(bodyParser.json())
+    .use(session({
       secret: 'yoursessionsecretkey',
       resave: false,
       saveUninitialized: true,
@@ -23,19 +24,69 @@ polka() // You can also use Express
       store: new FileStore({
         path: '.sessions'
       })
-  }))
-  .use(
-    compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware({
-      session: req => ({
-        user: req.session && req.session.user,
-        addresses: req.session && req.session.addresses,
-        token: req.session && req.session.token,
-        expires: req.session && req.session.expires
+    }))
+    .use(
+      compression({ threshold: 0 }),
+      sirv('static', { dev }),
+      sapper.middleware({
+        session: req => ({
+          user: req.session && req.session.user,
+          addresses: req.session && req.session.addresses,
+          token: req.session && req.session.token,
+          expires: req.session && req.session.expires
+        })
       })
-    })
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+    )
+    .listen(PORT, err => {
+      if (err) console.log('error', err);
+    });
+} else {
+  const DatastoreStore = require('@google-cloud/connect-datastore')(session);
+  polka() // You can also use Express
+    .use(bodyParser.json())
+    .use(session({
+      secret: 'yoursessionsecretkey',
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 3600000
+      },
+      store: new DatastoreStore({
+        kind: 'express-sessions',
+     
+        // Optional: expire the session after this many milliseconds.
+        // note: datastore does not automatically delete all expired sessions
+        // you may want to run separate cleanup requests to remove expired sessions
+        // 0 means do not expire
+        expirationMs: 0,
+     
+        dataset: new Datastore({
+     
+          // For convenience, @google-cloud/datastore automatically looks for the
+          // GCLOUD_PROJECT environment variable. Or you can explicitly pass in a
+          // project ID here:
+          projectId: process.env.GCLOUD_PROJECT,
+     
+          // For convenience, @google-cloud/datastore automatically looks for the
+          // GOOGLE_APPLICATION_CREDENTIALS environment variable. Or you can
+          // explicitly pass in that path to your key file here:
+          keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
+        })
+      })
+    }))
+    .use(
+      compression({ threshold: 0 }),
+      sirv('static', { dev }),
+      sapper.middleware({
+        session: req => ({
+          user: req.session && req.session.user,
+          addresses: req.session && req.session.addresses,
+          token: req.session && req.session.token,
+          expires: req.session && req.session.expires
+        })
+      })
+    )
+    .listen(PORT, err => {
+      if (err) console.log('error', err);
+    });
+}
